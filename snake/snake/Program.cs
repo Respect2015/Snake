b5b77c64066803ada706bd0fd45263f326f1bb68
Respect2015 <belczev2015@yandex.ru>
2015-11-18 23:40:25 +0300

    Процедурный подход. Функция Draw

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,23 +10,8 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1, y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
-
-            int x2 = 4, y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
+            Console.WriteLine("Hello world");
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
